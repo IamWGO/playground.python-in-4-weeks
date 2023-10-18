6be7ddee2472b0621bf118e11001cc58b4262827
iamWGO <gottlieb@gmail.com>
2023-10-18 14:18:29 +0200

    fix integer to roman logic

--- Week01-Python-Basic/hard1_2.py
+++ Week01-Python-Basic/hard1_2.py
@@ -2,29 +2,32 @@
 # https://leetcode.com/problems/roman-to-integer/
 
 def int_to_roman(num):
-    val = [
-        1000, 900, 500, 400,
-        100, 90, 50, 40,
-        10, 9, 5, 4, 1
-    ]
-    syms = [
-        "M", "CM", "D", "CD",
-        "C", "XC", "L", "XL",
-        "X", "IX", "V", "IV", "I"
-    ]
+    roman_dict = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    }
 
     roman_numeral = ''
-    i = 0
-    while num > 0:
-        for _ in range(num // val[i]):
-            roman_numeral += syms[i]
-            num -= val[i]
-        i += 1
+    for key, value in roman_dict.items():
+        # check only result > 0 (// floor)
+        if num // key > 0:
+            roman_numeral += value
+            num -= key
 
     return roman_numeral
 
 
 # Example usage:
-integer_value = 6
-roman_numeral = int_to_roman(integer_value)
-print("Roman numeral:", roman_numeral)
+integer_value = 14
+print("Roman numeral:", int_to_roman(integer_value))
